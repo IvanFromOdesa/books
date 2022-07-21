e3fc7fce4da92d234d7a3fcfd2532de5a0e740be
IvanFromOdesa <krylosov382@gmail.com>
2022-07-21 16:58:50 +0300

    Small Fixes

--- src/main/java/com/krylosov_books/books/BooksApplication.java
+++ src/main/java/com/krylosov_books/books/BooksApplication.java
@@ -1,7 +1,6 @@
 package com.krylosov_books.books;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
